07cf288409913a0cdbb26f8956ba9b42c84119db
SUBHADIP SINHA <119493894+subhadipsinha722133@users.noreply.github.com>
2024-08-03 23:38:20 +0530

    Add files via upload

---
+++ Face Recognition/Facial Recognition attendance system using python.py
@@ -0,0 +1,9 @@
+from tkinter import*
+from tkinteri import ttk
+from PIL import Image, ImageTk
+
+class Face_Recognition_System:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1530X790+0+0")
+        self.root.title(" Face Recognition System")
